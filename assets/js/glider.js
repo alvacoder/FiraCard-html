90dd1ebc724ba695a562c951189655578f66f230
ninyhorlah <58943705+ninyhorlah@users.noreply.github.com>
2021-05-02 15:56:36 +0100

    login updated

--- assets/js/glider.js
+++ assets/js/glider.js
@@ -1,11 +1,23 @@
-window.addEventListener('load', function(){
-    new Glider(document.querySelector('.glider'), {
-  slidesToShow: 3,
-  dots: '#dots',
-  draggable: true,
-  arrows: {
-    prev: '.glider-prev',
-    next: '.glider-next'
-  }
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".glider"), {
+    slidesToShow: 3,
+    dots: "#dots",
+    draggable: true,
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
+  });
 });
-} )
+
+window.addEventListener("load", function () {
+  new Glider(document.querySelector(".gliderMobile"), {
+    slidesToShow: 1,
+    dots: "#dots",
+    draggable: true,
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next",
+    },
+  });
+});
