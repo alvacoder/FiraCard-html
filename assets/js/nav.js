f896b3e1ecbc3411cb2c5b9d93934227a149c843
ninyhorlah <58943705+ninyhorlah@users.noreply.github.com>
2021-05-06 02:05:18 +0100

    index updated

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,29 +1,16 @@
-<<<<<<< HEAD
-const hideLink = document.querySelector('.hideLink')
-const hideBtn = document.querySelector('.hideBtn')
-const toggle = document.querySelector('.toggle')
+const toggle = document.querySelector(".toggle");
+const showMenu = document.querySelector(".navContentsMobile");
+const closeMenu = document.querySelector(".toggleClose");
 
-const handleToggle = () => {
-    console.log('toggle clicked');
-    alert("toggle clicked")
-} 
-=======
+const handleToggle = () => {};
+toggle.addEventListener("click", () => {
+  showMenu.style.display = "block";
+  closeMenu.style.display = "block";
+  toggle.style.display = "none";
+});
 
-const toggle = document.querySelector('.toggle')
-const close = document.querySelector('.close')
-const hideLink = document.querySelector('.hideLinks')
-const hideBtn = document.querySelector('.hideBtn')
-
-document.addEventListener('click', () => {
-    
-    if(toggle){
-        console.log('toggle');
-        // (toggle.style.display = 'none') && (close.style.display = 'block')
-        hideLink.style.display = 'block'
-        hideBtn.style.display = 'block'
-    } else if (toggle) {
-        hideBtn.style.display = 'none'
-    }
-})
-
->>>>>>> 93e33216743781ce4adf4ce46beaa4b3b12aa2eb
+closeMenu.addEventListener("click", () => {
+  toggle.style.display = "block";
+  closeMenu.style.display = "none";
+  showMenu.style.display = "none";
+});
